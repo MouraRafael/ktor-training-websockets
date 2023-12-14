95f2889fb516051127489c0490e73caf26dbfbe6
Rafael Moura <rafael.g.moura@hotmail.com>
2023-12-14 16:41:55 -0300

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "br.com.mouraf.tutorial-websockets-server"
---
+++ src/main/kotlin/br/com/mouraf/plugins/Routing.kt
@@ -0,0 +1,10 @@
+package br.com.mouraf.plugins
+
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun Application.configureRouting() {
+    routing {
+    }
+}
